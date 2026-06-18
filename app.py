bcb1f394bb2d9d58fc93c31653bc02a32b326acf
AparnaMounikau763 <105029885+AparnaMounikau763@users.noreply.github.com>
2026-06-19 01:14:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "DEV")
-
 @app.route("/")
 def home():
-    return f"Hello from {ENVIRONMENT} Environment"
+    return {
+        "environment": os.getenv("APP_ENV"),
+        "message": os.getenv("WELCOME_MESSAGE"),
+        "debug": os.getenv("DEBUG")
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
